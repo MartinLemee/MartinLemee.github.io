a79253d6a35fe31319da233f2df5aedeb05c4b64
Tintou <martin.lm@outlook.fr>
2019-11-05 18:09:49 +0100

    first commit dist

---
+++ precache-manifest.31e32500e824d8994ad477c3d9989748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be0b56f20d4d2e68374a",
+    "url": "/js/chunk-vendors.f0419988.js"
+  },
+  {
+    "revision": "1f1f8a2c07a66db453b1",
+    "url": "/js/chunk-2d0d0094.a4a1a2ea.js"
+  },
+  {
+    "revision": "5c5bdc1ce67f7d4ca8b6",
+    "url": "/js/app.bac5ce0c.js"
+  },
+  {
+    "revision": "21e719ca455fea5fb711",
+    "url": "/js/about.ab84148f.js"
+  },
+  {
+    "revision": "07bd96a14790838283992cd721dce62b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5bdc1ce67f7d4ca8b6",
+    "url": "/css/app.c893870f.css"
+  }
+];
